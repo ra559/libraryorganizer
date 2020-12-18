84920252e1ad93ed9ec1f83b9a5667a1ecc53e46
Robert Alberto <arobertadrian@gmail.com>
2020-12-17 23:22:59 -0500

    Fix: issue with template directory

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, render_template, request, url_for, redirect, session
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route("/")
