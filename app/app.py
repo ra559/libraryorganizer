36919de520166fe296e37f4fb55f43cd08a6e293
Robert Alberto <arobertadrian@gmail.com>
2020-12-17 23:29:50 -0500

    Fix: signup page

--- app/app.py
+++ app/app.py
@@ -8,5 +8,20 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/book")
+def book():
+    return render_template("book.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run()
