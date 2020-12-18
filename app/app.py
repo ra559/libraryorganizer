321c0f3ca7f2714e218ac95301fca7d89e41c56c
Robert Alberto <arobertadrian@gmail.com>
2020-12-17 22:50:54 -0500

    Task: added template dir with index

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, url_for, render_template, request, url_for, redirect, session
+
 app = Flask(__name__)
-app.secret_key='123'
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run()
