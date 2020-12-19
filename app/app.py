007485f635cf50887bea72ff61502b375f5f9e2b
LenTheDev <lhl7@njit.edu>
2020-12-19 00:39:58 -0500

    Task: Code to connect db and tables

--- app/app.py
+++ app/app.py
@@ -1,9 +1,35 @@
-from flask import Flask, url_for, render_template, request, url_for, redirect, session, abort
 from typing import List, Dict
 import mysql.connector
 import simplejson as json
+from flask import Flask, Response
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
+
+
+def liborg() -> List[Dict]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'liborg'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute('SELECT * FROM books AND users')
+    result = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return result
+
+@app.route('/')
+def index() -> str:
+    js = json.dumps(liborg())
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -13,7 +39,6 @@ def home():
 @app.route("/login", methods=["POST"])
 def login():
     return render_template("login.html")
-
 
 @app.route("/signup", methods=["POST"])
 def signup():
@@ -27,16 +52,14 @@ def book():
 def forgot():
     return render_template("forgot.html")
 
-
 @app.route("/user", methods=["POST", "GET"])
 def user():
     return render_template("user.html")
-
 
 @app.route("/booklist", methods=["POST", "GET"])
 def booklist():
     return render_template("booklist.html")
 
-
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0')
