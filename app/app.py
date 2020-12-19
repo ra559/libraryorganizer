88ebe18c791cb8bcae417a476dae4ed7d879f573
Robert Alberto <arobertadrian@gmail.com>
2020-12-19 11:49:30 -0500

    more work

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my key'
+mysql = MySQL(app)
+
+
 
 
 @app.route("/")
@@ -20,6 +24,7 @@ def signup():
         userDetails = request.form
         email = userDetails['email']
         password = userDetails['password']
+
     else:
         return render_template("signup.html")
 
@@ -48,4 +53,4 @@ def profile():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
