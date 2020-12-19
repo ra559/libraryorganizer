896c6989dd5cdf67193db2be66825434eb81c028
Robert Alberto <arobertadrian@gmail.com>
2020-12-19 00:27:34 -0500

    Task: working on DB

--- app/app.py
+++ app/app.py
@@ -1,27 +1,43 @@
-from flask import Flask, url_for, render_template, request, url_for, redirect, session, abort
+from flask import Flask, url_for, render_template, request, url_for, redirect, session, abort, flash
+from random import randint
+from flask_wtf import FlaskForm
 from typing import List, Dict
 import mysql.connector
 import simplejson as json
+from wtforms import StringField, PasswordField
+
+import random
+import string
+
+def random_string_generator(str_size, allowed_chars):
+    return ''.join(random.choice(allowed_chars) for x in range(str_size))
+
+chars = string.ascii_letters + string.punctuation
+size = 12
 
 app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = random_string_generator(size, chars)
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
     return render_template('index.html')
 
+
 @app.route("/login", methods=["POST"])
 def login():
     return render_template("login.html")
 
 
-@app.route("/signup", methods=["POST"])
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
     return render_template("signup.html")
+
 
 @app.route("/book", methods=["POST", "GET"])
 def book():
     return render_template("book.html")
+
 
 @app.route("/forgot", methods=["POST", "GET"])
 def forgot():
