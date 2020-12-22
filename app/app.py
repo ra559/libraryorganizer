9b203d55491d42fc86911f0e88782193a437e7db
LenTheDev <lhl7@njit.edu>
2020-12-22 00:26:05 -0500

    Commit: "Task: Fix Bug Errors In app.py file"

--- app/app.py
+++ app/app.py
@@ -1,31 +1,12 @@
-import re
 from typing import List, Dict
-
-import requests
-from flask import Flask, request, Response, redirect, url_for, session
+from flask import Flask, request, Response, redirect, url_for
 from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
-from authlib.integrations.flask_client import OAuth
-
+import requests
+import re
 app = Flask(__name__)
-app.secret_key = ("this is a secreted")
 mysql = MySQL(cursorclass=DictCursor)
-
-# OAuth
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id='90827938468-2nabe3i97f9p51m2hi8g5r2q70fph8a2.apps.googleusercontent.com',
-    client_secret='V-R0a4HVGiYZkPFAvE70T1Z-',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    access_params=None,
-    authorize_base_url='https://googleapis.com/oauth2/vi/',
-    api_base_url='https://googleapis.com/oauth2/vi/',
-    client_kwargs={'scope': 'openid email profile'},
-)
 
 app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -40,25 +21,12 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    google = oauth.create_client('google')  # create the google oauth client
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-
-@app.route('/authorize')
-def authorize():
-    google = oauth.create_client('google')  # create the google oauth client
-    token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
-    user_info = resp.json()
-    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
-    # Here you use the profile/user data that you got and query your database find/register the user
-    # and set ur own data in the session not the profile from google
-    session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
-    return redirect('/')
+    if request.method == 'POST':
+        return render_template('user.html')
+    else:
+        return render_template('login.html')
 
 
 @app.route('/list', methods=['GET'])
@@ -70,6 +38,17 @@ def list():
 
 
 # called by regristration page to register user
+@app.route('/insert', methods=['POST', 'GET'])
+def insert():
+    if request.method == 'POST':
+        user_email = request.form['user_email']
+        user_passwd = request.form['user_password']
+        cursor = mysql.get_db().cursor()
+        cursor.execute("""INSERT INTO users (user_email,user_passwd) VALUES (%s,%s)""", (user_email, user_passwd))
+        mysql.get_db().commit()
+        return render_template('login.html')
+    else:
+        return render_template('register.html')
 
 
 @app.route('/user', methods=["GET", "POST"])
@@ -103,8 +82,8 @@ def booksearch():
             publisher = item['publisher']
 
         return render_template('list2.html', isbn=isbn[0], title=title, author=author[0], lang=lang[0],
-                               genre=genre[0],
-                               publisher=publisher[0])
+                                   genre=genre[0],
+                                   publisher=publisher[0])
     except:
         return render_template('booknotfound.html')
 
@@ -138,8 +117,8 @@ def bookadd():
             genre = ['Undefined']
         publisher = item['publisher']
 
-        # validates the input for database eliminating all special chactacters.
-        # Example: Harry's will turn into Harrys
+# validates the input for database eliminating all special chactacters.
+# Example: Harry's will turn into Harrys
         isbn = re.sub('[^A-Za-z0-9 ]+', '', str(isbn[0]))
         title = re.sub('[^A-Za-z0-9 ]+', '', str(title))
         author = re.sub('[^A-Za-z0-9 ]+', '', str(author))
@@ -153,12 +132,6 @@ def bookadd():
         mysql.get_db().commit()
         return render_template('user.html')
 
-
-@app.route('/logout')
-def logout():
-    for key in list(session.keys()):
-        session.pop(key)
-    return redirect('/')
 
 
 if __name__ == "__main__":
